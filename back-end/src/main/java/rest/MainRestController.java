a01b6e8492760556aa31d7da103b42da2cd0ad0e
daangroot <groot.d@hotmail.nl>
2019-03-29 05:13:22 +0100

    allow all headers.

--- back-end/src/main/java/rest/MainRestController.java
+++ back-end/src/main/java/rest/MainRestController.java
@@ -17,7 +17,7 @@ import converters.CsvConverter;
 import converters.JsonConverter;
 import database.DatabaseConnector;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class MainRestController {
     private DatabaseConnector databaseConnector;
